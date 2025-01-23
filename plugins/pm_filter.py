2250311bbb65b823da53cad2d308ed20981e473a
Spideyofficial777 <gaminghatyar777@gmail.com>
2025-01-23 21:02:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,7 +36,7 @@ async def pm_search(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "sᴜᴘᴘᴏʀᴛ 😊", url=link
+                        "sᴜᴘᴘᴏʀᴛ 😊", url=MOVIE_GROUP_LINK
                     )
                 ]
             ]
