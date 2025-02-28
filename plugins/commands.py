c7f7b73cc17c065178a7d07b709465f52bac1bb0
Spideyofficial777 <gaminghatyar777@gmail.com>
2025-02-28 09:15:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,8 +13,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , ForceRep
 from database.ia_filterdb import Media, get_file_details, get_bad_files, unpack_new_file_id
 from database.users_chats_db import db
 from database.config_db import mdb
-from database.topdb import JsTopDB
-from database.jsreferdb import referdb
+from database.topdb import SpideyDB
+from database.spideyreferdb import referdb
 from plugins.pm_filter import auto_filter
 from utils import formate_file_name,  get_settings, save_group_settings, is_req_subscribed, get_size, get_shortlink, is_check_admin, get_status, temp, get_readable_time, save_default_settings
 import re
@@ -22,7 +22,7 @@ import base64
 from info import *
 import traceback
 logger = logging.getLogger(__name__)
-movie_series_db = JsTopDB(DATABASE_URI)
+movie_series_db = SpideyDB(DATABASE_URI)
 verification_ids = {}
 
 # CHECK COMPONENTS FOLDER FOR MORE COMMANDS
